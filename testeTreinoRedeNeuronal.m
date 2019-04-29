90d7956d40843b590965072f8557e7e105816afb
Pedro Franco <pedrofranco6@hotmail.com>
2019-04-29 10:10:35 +0100

    asdasd

--- testeTreinoRedeNeuronal.m
+++ testeTreinoRedeNeuronal.m
@@ -7,67 +7,42 @@ imageSize = 20;
 hogFeatures = 1;
 boundaries = 1;
 
-% hogFeaturesStrongest = 0;
-% wantedCornerPoints = 5;
-
-% TRAINING SETS
-
-% trainingSet = [];
-% tempImage = medfilt2(imread('Imagens/Formas_2/circle/107.png'),[8 8],'symmetric');
-% strongest = selectStrongest(detectHarrisFeatures(tempImage),wantedCornerPoints);
-% disp(strongest.Count);
-% teste = reshape(extractHOGFeatures(tempImage,strongest,'CellSize',[8 8]),1,[]);
-% asd = zeros(1,(wantedCornerPoints-strongest.Count)*36);
-% teste = [teste asd];
-% % hogFeaturesArray = [hogFeaturesArray zeros((wantedCornerPoints-strongest.Count)*36)];
-% trainingSet = vertcat(trainingSet,asd);
-
-% if hogFeaturesStrongest
-%     tempImage = medfilt2(imread(circleImageDatabase.ImageLocation{i}),[8 8],'symmetric');
-%     strongest = selectStrongest(detectHarrisFeatures(tempImage),wantedCornerPoints);
-%     hogFeaturesArray = reshape(extractHOGFeatures(tempImage,strongest,'CellSize',[8 8]),1,[]);
-%     hogFeaturesArray = [hogFeaturesArray zeros(1,(wantedCornerPoints-strongest.Count)*36)];
-%     trainingSet = vertcat(trainingSet,hogFeaturesArray);
-% end
+trainFunction = 'trainlm';
+% 'trainlm' % Levenberg-Marquardt
+% 'trainbr' % Bayesian Regularization
+% 'trainbfg' % BFGS Quasi-Newton
+% 'trainrp' % Resilient Backpropagation
+% 'trainscg' % Scaled Conjugate Gradient
+% 'traincgb' % Conjugate Gradient with Powell/Beale Restarts
+% 'traincgf' % Fletcher-Powell Conjugate Gradient
+% 'traincgp' % Polak-Ribiére Conjugate Gradient
+% 'trainoss' % One Step Secant
+% 'traingdx' % Variable Learning Rate Gradient Descent
+% 'traingdm' % Gradient Descent with Momentum
+% 'traingd' % Gradient Descent
 
 tic
 [trainingSet,targetTrainingSet] = datasetGenerator(datasetTrainingDir,numRotations,imageSize,hogFeatures,boundaries);
-[testingSet,targetTestingSet] = datasetGenerator(datasetTestingDir,numRotations,imageSize,hogFeatures,boundaries);
+[testingSet,targetTestingSet] = datasetGenerator(datasetTestingDir,0,imageSize,hogFeatures,boundaries);
 toc
+
 fprintf('\n');
 tic
-net = feedforwardnet(10);
-% net = patternnet(10);
-
-for t=0:10
-    disp(strcat('Rede:',num2str(t)));
+for t=1:10
+%     net = feedforwardnet((10),trainFunction);
+    net = fitnet(10,trainFunction);
+%     net = patternnet(10,trainFunction);
+%     net = cascadeforwardnet(10,trainFunction);
     
+    disp(strcat('Rede:',num2str(t)));
     net = train(net,trainingSet,targetTrainingSet);
     
-    out=sim(net,trainingSet);
-    r=0;
-    for i=1:length(out)
-        a = out(i);
-        b = targetTrainingSet(i);
-        if abs(a-b) < 0.01
-            r = r+1;
-        end
-    end
-    precisaoTreino = r/length(out)*100;
+    precisaoTreino=100-perform(net,targetTrainingSet,net(trainingSet));
     disp(strcat('Precisao Treino:',num2str(precisaoTreino)));
     
-    out=sim(net,testingSet);
-    r=0;
-    for i=1:length(out)
-        a = out(i);
-        b = targetTestingSet(i);
-        if abs(a-b) < 0.01
-            r = r+1;
-        end
-    end
-    
-    precisaoTeste = r/length(out)*100;
+    precisaoTeste=100-perform(net,targetTestingSet,net(testingSet));
     disp(strcat('Precisao Teste:',num2str(precisaoTeste)));
+    
     fprintf('\n');
 end
 toc
