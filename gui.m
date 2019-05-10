800090f8ef9d500a29b5288ff5507fbfeaf0c2b7
unknown <pedrofranco6@hotmail.com>
2019-05-10 02:00:01 +0100

    adaptacao visual para windows, e organizacao de codigo

--- gui.m
+++ gui.m
@@ -30,9 +30,9 @@ guidata(hObject, handles);
 function varargout = gui_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
-
-% --- Executes on button press in selectFolderBtn.
-function selectFolderBtn_Callback(hObject, eventdata, handles)
+% --- START *GENERATE DATASET* START ---
+% --- Executes on button press in selectFolderDatasetBtn.
+function selectFolderDatasetBtn_Callback(hObject, eventdata, handles)
 if ispc; lastSlash = '\'; else; lastSlash = '/'; end
 set(handles.imageDirectory, 'String', strcat(uigetdir(),lastSlash));
 
@@ -48,9 +48,10 @@ end
 
 % --- Executes during object creation, after setting all properties.
 function numRotations_CreateFcn(hObject, eventdata, handles)
-if ~ispc
-    set(hObject,'InnerPosition',[238.5 153.8077])
-    % 238.5 153.8077
+if ispc
+    set(hObject,'Position',[164.5 123.75 36.75 16.5]);
+else
+    set(hObject,'InnerPosition',[238.5 153.8077]);
 end
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
@@ -70,31 +71,20 @@ end
 
 % --- Executes during object creation, after setting all properties.
 function imageSize_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[165.0 105.0 36.0 16.5]);
+else
+    set(hObject,'InnerPosition',[238.5 153.8077]);
+end
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
-% --- Executes on button press in hogFeatures.
-function hogFeatures_Callback(hObject, eventdata, handles)
-
 % --- Executes on button press in boundaries.
 function boundaries_Callback(hObject, eventdata, handles)
 
-% --- Executes on button press in createDatasetBtn.
-function createDatasetBtn_Callback(hObject, eventdata, handles)
-if strcmp(get(handles.imageDirectory,'String'), ' ')
-    errordlg('Valid directory must be chosen');
-else
-    if get(handles.exportDataset,'Value') && strcmp(get(handles.datasetName,'String'),' ')
-        errordlg('Dataset name must be chosen');
-    else
-        wb = waitbar(.5,'Generating Dataset...');
-        [handles.trainingSet,handles.targetTrainingSet] = datasetGenerator(get(handles.imageDirectory,'String'),str2num(get(handles.numRotations,'String')),str2num(get(handles.imageSize,'String')),get(handles.hogFeatures,'Value'),get(handles.boundaries,'Value'),get(handles.exportDataset,'Value'),get(handles.datasetName,'String'));
-        guidata(hObject,handles);
-        delete(wb);
-        helpdlg('Dataset finished!');
-    end
-end
+% --- Executes on button press in hogFeatures.
+function hogFeatures_Callback(hObject, eventdata, handles)
 
 % --- Executes on button press in exportDataset.
 function exportDataset_Callback(hObject, eventdata, handles)
@@ -112,57 +102,220 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+% --- Executes on button press in createDatasetBtn.
+function createDatasetBtn_Callback(hObject, eventdata, handles)
+if strcmp(get(handles.imageDirectory,'String'), ' ')
+    errordlg('Valid directory must be chosen');
+else
+    if get(handles.exportDataset,'Value') && strcmp(get(handles.datasetName,'String'),' ')
+        errordlg('Dataset name must be chosen');
+    else
+        wb = waitbar(.5,'Generating Dataset...');
+        [handles.trainingSet,handles.targetTrainingSet] = datasetGenerator(get(handles.imageDirectory,'String'),str2num(get(handles.numRotations,'String')),str2num(get(handles.imageSize,'String')),get(handles.hogFeatures,'Value'),get(handles.boundaries,'Value'),get(handles.exportDataset,'Value'),get(handles.datasetName,'String'));
+        guidata(hObject,handles);
+        delete(wb);
+        helpdlg('Dataset finished!');
+    end
+end
+% --- END *GENERATE DATASET* END ---
+
+% --- START *IMPORT DATASET* START ---
 % --- Executes on button press in selectDatasetBtn.
 function selectDatasetBtn_Callback(hObject, eventdata, handles)
 set(handles.datasetDirectory, 'String', uigetfile('*.mat'));
 
-function edit8_Callback(hObject, eventdata, handles)
+% --- Executes on button press in importDatasetBtn.
+function importDatasetBtn_Callback(hObject, eventdata, handles)
+% --- END *IMPORT DATASET* END ---
+
+% --- START *NETWORK TRAINING* START ---
+% --- Executes during object creation, after setting all properties.
+function trainingFunctionText_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[4.714 18.938 27 1.062]);
+else
+    set(hObject,'InnerPosition',[4.714 18.938 15.714 1.062]);
+end
+
+% --- Executes on selection change in trainingFunction.
+function trainingFunction_Callback(hObject, eventdata, handles)
 
 % --- Executes during object creation, after setting all properties.
-function edit8_CreateFcn(hObject, eventdata, handles)
+function trainingFunction_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[32.2 19.077 15 1.385]);
+else
+    %     set(hObject,'InnerPosition',[4.714 18.938 15.714 1.062]);
+end
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
-% --- Executes on button press in importDataset.
-function importDataset_Callback(hObject, eventdata, handles)
+% --- Executes during object creation, after setting all properties.
+function numHiddenLayersText_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[4.714 16.846 27.4 1.077]);
+else
+    set(hObject,'InnerPosition',[4.714 16.625 22.857 1.063]);
+end
 
-% --- Executes on button press in checkbox7.
-function checkbox7_Callback(hObject, eventdata, handles)
-
-% --- Executes on button press in checkbox8.
-function checkbox8_Callback(hObject, eventdata, handles)
-
-function edit9_Callback(hObject, eventdata, handles)
+% --- Executes on selection change in numHiddenLayersDropdown.
+function numHiddenLayersDropdown_Callback(hObject, eventdata, handles)
+if get(handles.numHiddenLayersDropdown,'Value') == 2
+    set(handles.hiddenLayer1,'Value',1);
+    set(handles.hiddenLayer2,'Enable','on');
+    set(handles.hiddenLayer3,'Enable','off');
+elseif get(handles.numHiddenLayersDropdown,'Value') == 3
+    set(handles.hiddenLayer2,'Enable','on');
+    set(handles.hiddenLayer3,'Enable','on');
+else
+    set(handles.hiddenLayer1,'Value',1);
+    set(handles.hiddenLayer2,'Enable','off');
+    set(handles.hiddenLayer3,'Enable','off');
+end
 
 % --- Executes during object creation, after setting all properties.
-function edit9_CreateFcn(hObject, eventdata, handles)
+function numHiddenLayersDropdown_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[32.2 16.462 8.6 1.923]);
+else
+    set(hObject,'InnerPosition',[32.2 16.462 5 8.6 1.923]);
+end
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
-% --- Executes on button press in checkbox9.
-function checkbox9_Callback(hObject, eventdata, handles)
+% --- Executes during object creation, after setting all properties.
+function confHiddenLayerText_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[4.8 14.769 36.5 1.077]);
+else
+    set(hObject,'InnerPosition',[4.8 14.769 30.8 1.077]);
+end
 
-function edit10_Callback(hObject, eventdata, handles)
+% --- Executes when selected object is changed in confHiddenLayerSelection.
+function confHiddenLayerSelection_SelectionChangedFcn(hObject, eventdata, handles)
+selectedRadioButton = str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String'));
+set(handles.numNeurons,'String',num2str(handles.numNeuronsPerLayer(selectedRadioButton)));
+set(handles.activationFunction,'Value',find(contains(get(handles.activationFunction,'String'),handles.activFunctionPerLayer{selectedRadioButton})));
 
 % --- Executes during object creation, after setting all properties.
-function edit10_CreateFcn(hObject, eventdata, handles)
+function hiddenLayer1_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[5.7 0.312 5.857 1.062]);
+else
+    set(hObject,'InnerPosition',[2.714 0.312 5.857 1.062]);
+end
+
+% --- Executes during object creation, after setting all properties.
+function hiddenLayer2_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[13.3 0.312 5.857 1.062]);
+else
+    set(hObject,'InnerPosition',[11.857 0.312 5.857 1.062]);
+end
+
+% --- Executes during object creation, after setting all properties.
+function hiddenLayer3_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[21.2 0.312 5.857 1.062]);
+else
+    set(hObject,'InnerPosition',[21.286 0.312 5.857 1.062]);
+end
+
+% --- Executes during object creation, after setting all properties.
+function numNeuronsText_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[2.2 1.538 19.6 1.077]);
+else
+    set(hObject,'InnerPosition',[2.2 1.538 16.6 1.077]);
+end
+
+% --- Executes on numNeurons field edit.
+function numNeurons_Callback(hObject, eventdata, handles)
+selectedRadioButton = str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String'));
+% set(handles.numNeurons,'String',handles.numNeuronsPerLayer(selectedRadioButton));
+handles.numNeuronsPerLayer(selectedRadioButton) =  str2num(get(handles.numNeurons,'String'));
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function numNeurons_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[82.5 12 27.75 16.5]);
+else
+    set(hObject,'InnerPosition',[98.25 15.75]);
+end
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
 
-% --- Executes on button press in pushbutton11.
-function pushbutton11_Callback(hObject, eventdata, handles)
+% --- Executes during object creation, after setting all properties.
+function activationFunctionText_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[34 2.5 21.2 0.875]);
+else
+    set(hObject,'InnerPosition',[34.857 2.5 17.286 0.875]);
+end
 
-% --- Executes on button press in pushbutton12.
-function pushbutton12_Callback(hObject, eventdata, handles)
+% --- Executes on selection change in activationFunction.
+function activationFunction_Callback(hObject, eventdata, handles)
+activationFunctions = get(handles.activationFunction,'String');
+handles.activFunctionPerLayer{str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String'))} = cellstr(activationFunctions{get(handles.activationFunction,'Value')});
+% handles.activFunctionPerLayer(str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String')))
+guidata(hObject, handles);
 
-% --- Executes on button press in pushbutton13.
-function pushbutton13_Callback(hObject, eventdata, handles)
+% --- Executes during object creation, after setting all properties.
+function activationFunction_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
 
-% --- Executes on button press in pushbutton14.
-function pushbutton14_Callback(hObject, eventdata, handles)
+% --- Executes during object creation, after setting all properties.
+function trainingRepetitionsText_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[3.2 7.625 54 1.062]);
+else
+    set(hObject,'InnerPosition',[4.714 7.625 47.429 1.062]);
+end
+
+function trainingRepetitions_Callback(hObject, eventdata, handles)
+
+% --- Executes during object creation, after setting all properties.
+function trainingRepetitions_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[214.5 72 27.75 16.5]);
+else
+    set(hObject,'InnerPosition',[273 90 36.75 16.5]);
+end
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes on button press in trainNetworkBtn.
+function trainNetworkBtn_Callback(hObject, eventdata, handles)
+strings = get(handles.activationFunction,'String');
+teste = strings{get(handles.activationFunction,'Value')}
+
+% --- Executes during object creation, after setting all properties.
+function trainNetworkBtn_CreateFcn(hObject, eventdata, handles)
+if ispc
+    set(hObject,'Position',[50 0.625 16.8 1.688]);
+else
+    set(hObject,'InnerPosition',[53.286 0.625 13.857 1.688]);
+end
+% --- END *NETWORK TRAINING* END ---
+
+% --- START *TEST SAMPLES* START ---
+% --- Executes on button press in selectFolderTestSamplesBtn.
+function selectFolderTestSamplesBtn_Callback(hObject, eventdata, handles)
+
+% --- Executes on button press in importTestSamplesBtn.
+function importTestSamplesBtn_Callback(hObject, eventdata, handles)
+% --- END *TEST SAMPLES* END ---
+
+% --- START *TEST DRAWINGS* START ---
+% --- Executes on mouse press over axes background.
+function axesUserDraw_ButtonDownFcn(hObject, eventdata, handles)
 userDraw(handles);
 
 function userDraw(handles)
@@ -195,10 +348,6 @@ function done_pencil(src,evendata)
 set(gcf,'windowbuttonmotionfcn','')
 set(gcf,'windowbuttonupfcn','')
 
-% --- Executes on mouse press over axes background.
-function axesUserDraw_ButtonDownFcn(hObject, eventdata, handles)
-userDraw(handles);
-
 % --- Executes on button press in clearDraw.
 function clearDraw_Callback(hObject, eventdata, handles)
 cla(handles.axesUserDraw);
@@ -212,79 +361,4 @@ else
     drawing = frame2im(getframe(handles.axesUserDraw));
     set(handles.drawingTestResult,'String','Image is inconclusive...');
 end
-
-% --- Executes on selection change in popupmenu2.
-function popupmenu2_Callback(hObject, eventdata, handles)
-if get(handles.popupmenu2,'Value') == 2
-    set(handles.hiddenLayer1,'Value',1);
-    set(handles.hiddenLayer2,'Enable','on');
-    set(handles.hiddenLayer3,'Enable','off');
-elseif get(handles.popupmenu2,'Value') == 3
-    set(handles.hiddenLayer2,'Enable','on');
-    set(handles.hiddenLayer3,'Enable','on');
-else
-    set(handles.hiddenLayer1,'Value',1);
-    set(handles.hiddenLayer2,'Enable','off');
-    set(handles.hiddenLayer3,'Enable','off');
-end
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu2_CreateFcn(hObject, eventdata, handles)
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-% --- Executes on numNeurons field edit.
-function numNeurons_Callback(hObject, eventdata, handles)
-selectedRadioButton = str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String'));
-% set(handles.numNeurons,'String',handles.numNeuronsPerLayer(selectedRadioButton));
-handles.numNeuronsPerLayer(selectedRadioButton) =  str2num(get(handles.numNeurons,'String'));
-guidata(hObject, handles);
-
-% --- Executes during object creation, after setting all properties.
-function numNeurons_CreateFcn(hObject, eventdata, handles)
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-% --- Executes on selection change in activationFunction.
-function activationFunction_Callback(hObject, eventdata, handles)
-activationFunctions = get(handles.activationFunction,'String');
-handles.activFunctionPerLayer{str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String'))} = cellstr(activationFunctions{get(handles.activationFunction,'Value')});
-% handles.activFunctionPerLayer(str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String')))
-guidata(hObject, handles);
-
-% --- Executes during object creation, after setting all properties.
-function activationFunction_CreateFcn(hObject, eventdata, handles)
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-% --- Executes on button press in trainNetworkBtn.
-function trainNetworkBtn_Callback(hObject, eventdata, handles)
-strings = get(handles.activationFunction,'String');
-teste = strings{get(handles.activationFunction,'Value')}
-
-% --- Executes when selected object is changed in confHiddenLayerSelection.
-function confHiddenLayerSelection_SelectionChangedFcn(hObject, eventdata, handles)
-selectedRadioButton = str2num(get(get(handles.confHiddenLayerSelection,'SelectedObject'),'String'));
-set(handles.numNeurons,'String',num2str(handles.numNeuronsPerLayer(selectedRadioButton)));
-set(handles.activationFunction,'Value',find(contains(get(handles.activationFunction,'String'),handles.activFunctionPerLayer{selectedRadioButton})));
-
-
-% --- Executes on selection change in popupmenu5.
-function popupmenu5_Callback(hObject, eventdata, handles)
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu5_CreateFcn(hObject, eventdata, handles)
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
-function edit12_Callback(hObject, eventdata, handles)
-
-% --- Executes during object creation, after setting all properties.
-function edit12_CreateFcn(hObject, eventdata, handles)
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
+% --- END *TEST DRAWINGS* END ---
